d36de76969646e9e996138737b56bf22cd063d6f
phrb <pedro.bruel@gmail.com>
2015-11-12 12:43:11 -0200

    Server now changes working directory to user's.

--- commands.py
+++ commands.py
@@ -152,6 +152,10 @@ def load_interface(conn, command):
                                                             SERVER_STATUS))
         if os.path.isfile(command[1]):
             sys.path.insert(1, os.path.dirname(command[1]))
+
+            logger.info("Changing working directory to {0}".format(os.path.dirname[1]))
+            os.chdir(os.path.dirname(command[1]))
+
             # Loads user module as user_module, using the file path
             # received from the user.
             user_module = imp.load_source("user_module", command[1])
